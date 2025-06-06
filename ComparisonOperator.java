92332f6f56bbe83cf03ef3c49557cc2ff72a68e3
mohsinp1998 <mohsinp9893@gmail.com>
2025-06-06 16:17:48 +0530

    Add files via upload

---
+++ ComparisonOperator.java
@@ -0,0 +1,18 @@
+package myfirstpackage;
+
+public class ComparisonOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 20;
+
+System.out.println("is a equal to b "+ (a==b));
+System.out.println("is not equal to b "+ (a!=b));
+System.out.println("is greater than b "+ (a>b));
+System.out.println("is less than to b "+ (a<b));
+System.out.println("is greater or equal to b "+ (a>=b));
+System.out.println("is less or equal to b "+ (a<=b));
+	}
+
+}
