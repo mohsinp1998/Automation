9e7afd76c6e8d18eb26cb979df1df0112f6c124b
mohsinp1998 <mohsinp9893@gmail.com>
2025-06-09 20:59:45 +0530

    Add files via upload

---
+++ LearningOperatorsAndDatatypes.java
@@ -0,0 +1,15 @@
+package myfirstpackage;
+
+public class LearningOperatorsAndDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int Maths = 65;
+        int Science = 70;
+        int English = 58;
+        int Total = 193 ;
+        System.out.println("Total Marks is "+(Maths+Science+English));
+        System.out.println("Avg Marks is "+(Total/3));
+	}
+
+}
